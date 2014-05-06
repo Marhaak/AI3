97a8efd71d45517b78af503b6288dc408a1fe6c4
LuckyDjuk <alf.hammerseth@gmail.com>
2014-05-06 11:40:46 +0200

    All the comments!

--- AI4/AI4/Source.cpp
+++ AI4/AI4/Source.cpp
@@ -1,12 +1,8 @@
-
- 
 #include <stdio.h> 
 #include <stdlib.h>
 #include <iostream>
-//#include "Include.h"
 #include "SDL.h"
 #include "SDL_image.h"
-
 
 bool done;
 int board[3][3];
@@ -17,60 +13,53 @@ SDL_Texture* textureSheet[2];		// Array with the textures
 SDL_Window*   window;				// The window
 SDL_Renderer* renderer;				// The renderer
 
-void ApplySurface(int x, int y, SDL_Texture *tex, SDL_Renderer *rend) {
+void ApplySurface(int x, int y, SDL_Texture *tex, SDL_Renderer *rend) { // Parameters, x and y, define position in the projected window.
 	SDL_Rect pos;
     pos.x = x;
     pos.y = y;
-	SDL_QueryTexture(tex, NULL, NULL, &pos.w, &pos.h); 
+	SDL_QueryTexture(tex, NULL, NULL, &pos.w, &pos.h); // Setup ups the image and shows it.
     SDL_RenderCopy(rend, tex, NULL, &pos);
 }
 
-SDL_Texture* loadImage(char* _file) {
+SDL_Texture* loadImage(char* _file) { // Returns a pointer to the texture.
 	SDL_Texture* tex = nullptr;
 	tex = IMG_LoadTexture(renderer, _file);
-    if (tex == nullptr) {
+    if (tex == nullptr) { // Checks the pointer and print error.
 		std::cout<<"Failed to load image: "<< _file<<" "<<  IMG_GetError()<< " ";
 		std::cin.get();
 	}
-	
     return tex;
 }
 
 bool InitSDL() {
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1){
-		//cout << SDL_GetError() << endl;
 		return false;
 	}
 
 	// Creating the window
 	window = SDL_CreateWindow("Tick Tack Toe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, winXSize, winYSize, NULL);
     if (window == nullptr){
-        //cout << SDL_GetError() << "\n";
 		return false;
     }
 
 	// Creating the renderer
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr){
-        //cout << SDL_GetError() << endl;
 		return false;
     }
 
-
+	// Loads all the pictures.
 	textureSheet[0] = loadImage("blank.png");
 	textureSheet[1] = loadImage("O.png");
 	textureSheet[2] = loadImage("X.png");
-
 
 	// Everything went ok
 	return true;
 }
 
+void select(int &X, int &Y) { // Retrieves the clicked position and prepares it for usage.
 
-
-void select(int &X, int &Y) {
-	
 	bool click = false;
 	SDL_Event _event;
 
@@ -90,13 +79,10 @@ void select(int &X, int &Y) {
 	}
 }
 
-
 void Draw(int _x, int _y, int _i) {
-
 	// Draws to back buffer
 	ApplySurface(_x, _y, textureSheet[_i], renderer);
 }
-
 
 int X(int pos) {
 	return pos % 3;
@@ -132,13 +118,13 @@ void printIntro() {
 	printf("                                                      \n");
 }
 
-void setup() {
-
-	printf("Input starting player: ");
-	scanf_s("%d", &player);
+void setup() { // Prepares the board and set ups the order of plays for the algorithm.
+	printf("Input starting player(0 = AI first, 1 = Human first): "); // 0 = Ai first, 1 = Human first.
+	scanf_s("%d", &player); 
 	if (player != 0) {
 		player = 1;
 	}
+
 	int i, j;
 	for (i = 0; i < 3; i++) { 
 		for (j = 0; j < 3; j++) {
@@ -149,49 +135,24 @@ void setup() {
 	done = false;
 	
 	printf("\n\tStart Game...\n\n\n");
-	
 }
   
-void printBoard() {
+void printBoard() { // Prints the image related to the value in the array.
 	int i, j;
-	SDL_RenderClear(renderer);
+	SDL_RenderClear(renderer); // Clears the screen before projecting images.
 	for (i = 0; i < 3; i++) {
-		printf("\t");
 		for (j = 0; j < 3; j++) {
-
-			if (board[i][j] == -1) {
-				printf(" ");
-				Draw(i * 100, j * 100, board[i][j]+1);
-			}
-			
-			if (board[i][j] == 0) {
-
-				printf("O");
-				Draw(i * 100, j * 100, board[i][j]+1);
-			}
-			
-			if (board[i][j] == 1) {
-
-				printf("X");
-				Draw(i * 100, j * 100, board[i][j]+1);
-			}
-			
-			if (j != 2)
-			printf("|");
+			Draw(i * 100, j * 100, board[i][j]+1); // Draws the designated image.
 		}
-		printf("\n");
-		if (i != 2)
-		printf("\t-----\n");
-		
 	}
-	printf("\n\n");
-	SDL_RenderPresent(renderer);
+	SDL_RenderPresent(renderer); // Renders the prepared images.
 }
  
 int checkGame() {
 	int i, j;
-	for (i = 0; i < 3; i++) {
 
+	// Checking if the game is won Vertically
+	for (i = 0; i < 3; i++) {
 		int fl = board[i][0];
 		for (j = 1; j < 3; j++) {
 			if (board[i][j] != fl) {
@@ -205,8 +166,8 @@ int checkGame() {
 		return 1;
 	}
  
+	// Check if the game is won Horizontal
 	for (i = 0; i < 3; i++) {
-
 		int fl = board[0][i];
 		for (j = 1; j < 3; j++) {
 			if (board[j][i] != fl) {
@@ -219,19 +180,18 @@ int checkGame() {
 		winner = fl;
 		return 1;
 	}
- 
+	
+	// Checking the game for diagonal winning
 	if (board[0][0] == board[1][1] && board[2][2] == board[1][1] && board[0][0] != -1) {
-
 		winner = board[0][0];
 		return 1;
-	}
- 
+	} 
 	if (board[0][2] == board[1][1] && board[2][0] == board[1][1] && board[1][1] != -1) {
-
 		winner = board[1][1];
 		return 1;
 	}
  
+	// If there is a free spot left
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
 			if (board[i][j] == -1) {
@@ -292,25 +252,25 @@ int testGame() {
  
 void playerPlay() {
 
+	// Print the board
 	printBoard();
-	printf("Select a box to play in (X Y): ");
-	int x; int y;
 	
-	select(x, y);
-	while (board[y][x] != -1) {
-
-		printf("Box aready taken...\n");
+	// Register the spot to play in.
+	int x; int y;
+	do{
+		printf("Click on a free square to play in.\n");
 		select(x, y);
-	}
+	}while (board[y][x] != -1);
+
+	// Change the selected square
 	set(x, y, player);
 	printf("Played in: %d %d\n", x, y);
 }
   
 int AlphaBetaNegamax(int who, int &move, int a, int b) {
 	
-	int D = testGame();
+	int D = testGame(); // Checks the state of the game.
 	if (D != 2) {
-
 		if (D == -1) {
 			return 0;
 		}
@@ -323,13 +283,11 @@ int AlphaBetaNegamax(int who, int &move, int a, int b) {
 	int i, flag = 0;
 	for (i = 0; i < 9; i++) {
 		if (board[Y(i)][X(i)] == -1) {
-
 			set(i, who);
 			int tmp;
 			int score = -AlphaBetaNegamax(1 - who, tmp, -b, -a);
 			unset(i);
 			if (score >= b) {
-
 				move = i;
 				return score;
 			}
@@ -343,16 +301,16 @@ int AlphaBetaNegamax(int who, int &move, int a, int b) {
 	return a;
 }
  
-void aiPlay() {
+void aiPlay() { // Ai calculates his move and plays.
   int bx = 0;
-  AlphaBetaNegamax(player, bx, -1, 1);
-  set(bx, player);
+  AlphaBetaNegamax(player, bx, -1, 1); // Calculates optimal move. Maximizes own score, minimazes human score.
+  set(bx, player); // Sets the Ai's move.
   printf("Played in: %d %d\n", X(bx), Y(bx));
 }
  
 void play() {
-	if (player == 1) {
 
+	if (player == 1) {
 		playerPlay();
 	} else {
 		aiPlay();
@@ -360,30 +318,30 @@ void play() {
 }
  
 void printOutro() {
-
-	// printIntro();
+	// Prints the winner of the game.
 	if( winner == 0) {
 		printf("Game Winner: AI");
 	} else {
-
 		printf("Game Winner: Player");
 	}
-	
 }
 
 int main(int argc, char* argv[]) {
-
+	// Initiates SDL
 	if(!InitSDL()) {
-
 		printf("Error!");
 		return 0;
 	}
-	bool restart = true;
-	while(restart) {
+	
+	// Play game!
+	bool restart;
+	do{
+		// Initiates game
 		printIntro();
 		setup();
-		while (!done) {
 
+		// While game is still playing
+		while (!done) {
 			play();
 			player = 1 - player;
 			if (checkGame()) {
@@ -392,16 +350,20 @@ int main(int argc, char* argv[]) {
 			}
 			printBoard();
 		}
+
+		// After game have ended, print winner and ask for restart.
 		printOutro();
 		char holder;
 		printf("\nDo you want to try again? Y/N ");
 		scanf_s(" %c", &holder, 1);
+
+		// If yes, set the restart bool.
 		if(toupper(holder) != 'Y') {
 			restart = false;
 		} else {
-
-			printf("\n\n");
+			restart = true;
+			system("CLS");
 		}
-	}
+	} while (restart);
 	return 0;
 }
